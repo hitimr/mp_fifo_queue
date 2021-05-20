f099719046b2b4e995298c606f105cd6317d5728
hitimr <hitimr@gmail.com>
2021-05-20 20:56:18 +0200

    ring buffer compiles

--- src/SC_Queue.hpp
+++ src/SC_Queue.hpp
@@ -6,12 +6,66 @@
 #include "common.h"
 
 
+template <class T>
+class RingBuffer
+{
+    size_t capacity, head, tail;
+    std::vector<T> data;
+
+    public:
+        RingBuffer(size_t new_capacity)
+        {
+            assert(new_capacity > 0);
+
+            capacity = new_capacity;
+            head = 0;
+            tail = 0;
+
+            data.clear();
+            data.resize(capacity);
+        }
+
+        void push(T x)
+        {
+            data[tail] = x;
+            tail++;
+
+            if(tail >= capacity)
+            {
+                tail = 0;
+            }
+        }
+
+        T pop()
+        {
+            T x = data[head];
+            head++;
+            if(head >= capacity)
+            {
+                head = 0;
+            }
+        }
+
+        size_t size()
+        {
+            if(head <= tail)
+            {
+                return tail - head;
+            }
+            else
+            {
+                return head - tail;
+            }
+        }
+};
+
 typedef struct SCQ_Element_t
 {
     int cycle;
     bool isSafe;
     int index;
 } SCQ_Element;
+
 
 class SC_Queue
 {
